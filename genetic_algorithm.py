dd8172daadeb234fe71c518fa1c6cf8e53263032
pranav-kirsur <pranav.kirsur@gmail.com>
2020-03-28 20:56:44 +0530

    ga changes

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -65,11 +65,14 @@ MATING_POOL_SIZE = 8
 # Size of the population
 population_size = (POPULATION_SIZE, 11)
 
+init_array = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
+              6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+
+
 
 def get_fitness(population):
     fitness_array = np.zeros((POPULATION_SIZE, 1))
     for member_index in range(POPULATION_SIZE):
-        print(list(population[member_index, :]))
         err_arr = get_errors(SECRET_KEY, list(population[member_index, :]))
         fitness_array[member_index] = err_arr[1]
     return fitness_array
@@ -87,7 +90,7 @@ def get_mating_pool(population, fitness):
     mating_pool = pop_fit[np.random.choice(
         POPULATION_SIZE, size=MATING_POOL_SIZE, replace=False, p=probablities)]
 
-    print(mating_pool)
+    np.random.shuffle(mating_pool)
     return mating_pool
 
 
@@ -104,17 +107,19 @@ def crossover(parents, children_shape):
         parent_2_index = (i+1) % parents.shape[0]
 
         # Crossover for ith child
-        children[i, 0: crossover_index] = parents[parent_1_index,
-                                                  0: crossover_index]
-        children[i, crossover_index:] = parents[parent_2_index, crossover_index:]
-        children[i, 0: ] = np.divide((np.multiply(parents[parent_1_index,0: ],parent_2_index+1)  +  np.multiply(parents[parent_2_index, 0:],parent_1_index+1)),parent_1_index+parent_2_index+2)
+        # children[i, 0: crossover_index] = parents[parent_1_index,
+        #                                           0: crossover_index]
+        # children[i, crossover_index:] = parents[parent_2_index, crossover_index:]
+        w1 = parents[parent_1_index, 11] + 1
+        w2 = parents[parent_2_index, 11] + 1
+        children[i, 0: ] = np.divide((np.multiply(parents[parent_1_index,0: ],w2)  +  np.multiply(parents[parent_2_index, 0:],w1)),w1 + w2)
 
-    return children[:,0:11]
+    return children[:, 0:11]
 
 
 def mutation(children):
-    random = np.random.uniform(low=-1, high=1, size=children.shape)
-    mutated_children = children + random
+    random = np.random.uniform(low = -2.0, high=2.0, size=children.shape)
+    mutated_children = np.multiply(children ,random)
     mutated_children = np.clip(mutated_children, -10, 10)
     return mutated_children
 
@@ -122,9 +127,11 @@ def mutation(children):
 # Creating the initial population randomly
 population = np.random.uniform(low=-10, high=10, size=population_size)
 
+population[0, :] = init_array
+
 
 # Number of generations
-NUMBER_OF_GENERATIONS = 20
+NUMBER_OF_GENERATIONS = 10
 
 # Run genetic algo
 for generation in range(NUMBER_OF_GENERATIONS):
@@ -135,7 +142,7 @@ for generation in range(NUMBER_OF_GENERATIONS):
     fitness_of_population = get_fitness(population)
 
     # Submit the current best and print it
-    mini = np.argmin(fitness_of_population)
+    # mini = np.argmin(fitness_of_population)
     # submit(SECRET_KEY, list(population[mini, :]))
     print("Current best:", np.min(fitness_of_population))
 
